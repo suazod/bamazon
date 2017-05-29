6b09a41c60e6abbf657a917e9268f723b08ef75b
Danny Suazo <dannysuazor@yahoo.com>
2017-05-29 02:39:27 -0500

    Add files via upload

--- bamazon/bamazonCustomer.js
+++ bamazon/bamazonCustomer.js
@@ -1,1 +1,44 @@
 //starter code
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var table = require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "test",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  itemSearch();
+});
+
+     connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.table(res)
+  });
+
+
+
+var itemSearch = function() {
+  inquirer.prompt({
+    name: "item_id",
+    type: "input",
+    message: "What is the item ID?"
+  }).then(function(answer) {
+    var query = "SELECT item_id FROM products WHERE ?";
+    connection.query(query, { artist: answer.name }, function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log("item_id: " + res[i].item_id);
+      }
+      itemSearch();
+    });
+  });
+};
